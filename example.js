19ebf35b68e9ac5ba9a4bc9c07efcd03e6707a09
Xiaozhangting520 <152702323+Xiaozhangting520@users.noreply.github.com>
2023-12-06 02:50:18 +0800

    Update example.js

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 // example.js
 
 // 定义JavaScript函数，用于按钮点击时跳转到另一个页面
-function redirectToAnotherPage() {
+function redirectToAnotherPage(https://github.com/Xiaozhangting520/Xiaozhangting520.github.io/blob/482de2aaa531ae4ebcece322bae98add5039d4dd/%E9%A1%B9%E7%9B%AE%E5%8C%BA.html) {
   // 在这里设置要跳转的页面的URL
   window.location.href = '项目区.html';
 }
